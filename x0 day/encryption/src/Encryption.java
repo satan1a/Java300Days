8070e56b16d61b5dfe5564c6fe0fe3f43f3eaf12
Toky <15988388619@163.com>
2018-09-13 22:01:07 +0800

    Initial commit

---
+++ x0 day/encryption/src/Encryption.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * 本项目是对Java使用异或加解密字符串的练习
+ * 知识点：toCharArray 方法，异或运算
+ */
+public class Encryption {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入你要加密或解密的字符串：");
+        String password = scan.nextLine();
+        char[] array = password.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (char) (array[i] ^ 20000);
+
+        }
+
+        System.err.println("最终字符串为：" + new String(array));
+
+    }
+}
