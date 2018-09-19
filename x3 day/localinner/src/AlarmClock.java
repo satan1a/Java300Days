1fa6dc57decfb858508b032cb7064af62baf130e
toky <979564561@qq.com>
2018-09-19 18:19:53 +0800

    第三天 alibaba
    alibaba coding guideline

--- x3 day/localinner/src/AlarmClock.java
+++ x3 day/localinner/src/AlarmClock.java
@@ -4,34 +4,56 @@ import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * AlarmClock
+ * @author satan1a
+ * @date 2018/9/18
+ */
 public class AlarmClock {
-    private int delay;  // 延迟时间
-    private boolean flag;   // 启动计时的标志
+    /**
+     * 延迟时间
+      */
+    private int delay;
+    /**
+     * 启动计时的标志
+      */
+    private boolean flag;
 
-    public AlarmClock(int delay, boolean flag) {    // 构造器 constructor 初始化域
+    /**
+     * 构造器，初始化域
+     * @param delay
+     * @param flag
+     */
+    public AlarmClock(int delay, boolean flag) {
         this.delay = delay;
         this.flag = flag;
 
     }
 
     public void start() {
-
-        class Printer implements ActionListener {  // 定义一个内部类并实现动作监听器
+        /**
+            定义一个内部类并实现动作监听器
+         */
+        class Printer implements ActionListener {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("k:m:s");  // 设置显示的日期格式
-                String resultTime = simpleDateFormat.format(new Date());  // 获得当前时间（以simpleDateFormat格式）
-                System.out.println("现在的时间是;" + resultTime);  // 显示当前时间
-                if (flag) {  // 根据标志判断是否beep, 标志在定义时设置
+                // 设置显示的日期格式
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("k:m:s");
+                // 获得当前时间（以simpleDateFormat格式）
+                String resultTime = simpleDateFormat.format(new Date());
+                // 显示当前时间
+                System.out.println("现在的时间是;" + resultTime);
+                // 根据标志判断是否beep, 标志在定义时设置
+                if (flag) {
                     Toolkit.getDefaultToolkit().beep();
-
                 }
 
             }
 
         }
-        new javax.swing.Timer(delay, new Printer()).start(); // 特别注意：此栗子中使用的是 swing 中的 Timer，具体见 README。
+        // 特别注意：此栗子中使用的是 swing 中的 Timer，具体见 README。
+        new javax.swing.Timer(delay, new Printer()).start();
 
     }
 
--- x3 day/localinner/src/LocalInner.java
+++ x3 day/localinner/src/LocalInner.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
 
+/**
+ * LocalInner
+ * @author satan1a
+ * @date 2018/9/18
+ */
 public class LocalInner {
     public static void main(String[] args) {
         AlarmClock clock = new AlarmClock(1000, true);
