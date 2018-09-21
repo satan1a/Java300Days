ae43e32f44995e477453371e366a5201509552a1
toky <979564561@qq.com>
2018-09-21 10:47:20 +0800

    第 4 天

--- x4 day/formatter/src/PrcFormatter.java
+++ x4 day/formatter/src/PrcFormatter.java
@@ -1,3 +1,31 @@
-public class PrcFormatter {
+import java.util.Formattable;
+import java.util.Formatter;
 
+public class PrcFormatter {
+    /**
+     * 实现 Formattable 接口
+     */
+    public static class Formatter implements Formattable {
+
+        @Override
+        public void formatTo(java.util.Formatter formatter, int flags, int width, int precision) {
+            formatter.format("我是 Formattable 接口的实现类");
+
+        }
+    }
+
+
+    /**
+     * 主函数
+     */
+    public static void main(String[] args) {
+        PrcFormatter.Formatter prc = new PrcFormatter.Formatter();
+
+        Formatter formatter = new Formatter();
+
+        System.out.printf(String.format("大家好，我叫：%1$s，今年：%2$-,( 20.3f岁。%1$s是%3$s的爸爸","小明",-255555555.2333333f,"小小明"));
+
+    }
 }
+
+
