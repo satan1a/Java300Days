28f0c5af2c8e497a9f640e0db12c4a179c61c124
toky <979564561@qq.com>
2018-09-29 19:04:59 +0800

    第 12 天

---
+++ x12 day/prcLambda/src/PrcLambda.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * PrcLambda
+ * 对 Lambda 匿名函数的练习
+ * @author satan1a
+ * @date 2018_9_29
+ */
+public class PrcLambda {
+
+
+
+    public static void main(String[] args) {
+
+        String string = "Satania,tom,,jim,Berry,kitty,Anna";
+        // String 转化为 ArrayList 列表的方法：第一步，用 split 将字符串分割为字符数组； 第二步，使用 Arrays 的 asList 方法，将数组转化为列表
+        ArrayList<String> stringArrayList = new ArrayList<String>(Arrays.asList(string.split(",")));
+
+        // 定义原始字符数组
+        String[] stringsList = string.split(",");
+
+        // 按原始顺序输出字符串数组中的字符串元素
+        System.out.printf("按原始顺序输出字符串数组中的字符串元素: \n");
+        for (String s: stringsList) {
+            System.out.printf("");
+
+        }
+
+        // 按自己实现的 Comparator 来 sort()
+        Arrays.sort(stringsList, new LengthComparator());
+
+        for (String s: stringsList) {
+            System.out.printf(s.toUpperCase() + " ");
+
+        }
+
+    }
+}
+
+class LengthComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        // 根据字符串长度来排序
+        return Integer.compare(o1.length(), o2.length());
+
+    }
+}
+
+
