414aa87e7f9423b5633535301795334a411c739e
Toky <15988388619@163.com>
2018-10-09 20:45:29 +0800

    第 22 天

--- x22 day/prcProperties/README.md
+++ x22 day/prcProperties/README.md
@@ -3,3 +3,20 @@
 - 用来配置应用程序的一些信息（较少）
 - 由键值对构成，且键值对都是字符串
 - 读和写：https://blog.csdn.net/zzq58157383/article/details/6763659
+- 方法：  
+1. String getProperty(String key)  
+ 用指定的键在此属性列表中搜索属性。
+2. String getProperty(String key, String defaultProperty)  
+用指定的键在属性列表中搜索属性。
+3. void list(PrintStream streamOut)  
+ 将属性列表输出到指定的输出流。
+4. void list(PrintWriter streamOut)  
+将属性列表输出到指定的输出流。
+5. void load(InputStream streamIn) throws IOException  
+ 从输入流中读取属性列表（键和元素对）。
+6. Enumeration propertyNames( )  
+按简单的面向行的格式从输入字符流中读取属性列表（键和元素对）。
+7. Object setProperty(String key, String value)  
+ 调用 Hashtable 的方法 put。
+8. void store(OutputStream streamOut, String description)  
+ 以适合使用  load(InputStream)方法加载到 Properties 表中的格式，将此 Properties 表中的属性列表（键和元素对）写入输出流。
--- x22 day/prcProperties/src/PrcProperties.java
+++ x22 day/prcProperties/src/PrcProperties.java
@@ -1,7 +1,9 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Iterator;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * PrcProperties
@@ -12,18 +14,50 @@ import java.util.Properties;
  */
 public class PrcProperties {
 
-    public static void main(String[] args) throws Exception {
-        // Properties 属性集合对象
-        Properties prop = new Properties();
-        // 属性文件输入流
-        FileInputStream fileInputStream = new FileInputStream("prop.properties");
+//    public static void main(String[] args) throws Exception {
+//        // Properties 属性集合对象
+//        Properties prop = new Properties();
+//        // 属性文件输入流
+//        FileInputStream fileInputStream = new FileInputStream("prop.properties");
+//
+//        // 将属性文件输入流加载到 Properties 属性集合对象中，抛出IOException异常
+//        prop.load(fileInputStream);
+//        // 关闭流
+//        fileInputStream.close();
+//
+//        FileOutputStream fileOutputStream = new FileOutputStream("prop.properties");
+//        fileOutputStream.close();
+//    }
 
-        // 将属性文件输入流加载到 Properties 属性集合对象中，抛出IOException异常
-        prop.load(fileInputStream);
-        // 关闭流
-        fileInputStream.close();
+    public static void main(String[] args) {
+        Properties capitals = new Properties();
+        Set states;
+        String str;
 
-        FileOutputStream fileOutputStream = new FileOutputStream("prop.properties");
-        fileOutputStream.close();
+        capitals.put("Illinois", "Springfield");
+        capitals.put("Missouri", "Jefferson City");
+        capitals.put("Washington", "Olympia");
+        capitals.put("California", "Sacramento");
+        capitals.put("Indiana", "Indianapolis");
+
+        // Show all the states and capitals (like a hashtable)
+        states = capitals.keySet();
+        Iterator iterator = states.iterator();
+
+        while (iterator.hasNext()) {
+            str = (String)iterator.next();
+            System.out.println(String.format("The capital of %s is %s.",
+                    str, capitals.getProperty(str)));
+
+        }
+        System.out.println();
+
+        // look for state not in list -- specify default
+        str = capitals.getProperty("Florida", "Not Found");
+        System.out.println(String .format("The capital of Florida is %s.",
+                str));
+
     }
+
 }
+
