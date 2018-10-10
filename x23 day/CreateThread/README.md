7668375a3b9a69bce62cadcf986d08b5bf655f41
satan1a <979564561@qq.com>
2018-10-10 09:50:36 +0800

    第 23 天

--- x23 day/CreateThread/README.md
+++ x23 day/CreateThread/README.md
@@ -1,12 +1,15 @@
 # 多线程篇 - 创建多线程
 - 多线程概念：一个程序同时运行多个程序块，比如同时运行 选择语句、循环语句，和其它语句。
-- 创建多线程有 2（3）种方法：  
+- 创建多线程有 3 种方法：  
     - 继承 Thread 类 (extends Thread) 
     - 实现 Runnable 接口(implements Runnable)
-    - 【扩展内容】实现 Callable 接口
-- 
+    - 实现 Callable、Future 接口
+- 今日练习中，使用 new Thread 新建线程性能差 且 缺乏统一管理和更多的功能，应使用 Java线程池，明日补齐 线程池
+内容 https://blog.csdn.net/u011974987/article/details/51027795
  
  
  
  
- - 参考：https://blog.csdn.net/evankaka/article/details/44153709#t0
+ - 参考： 
+    - https://blog.csdn.net/evankaka/article/details/44153709#t0
+    - http://www.runoob.com/java/java-multithreading.html
--- x23 day/CreateThread/src/ImplementsRunnable.java
+++ x23 day/CreateThread/src/ImplementsRunnable.java
@@ -1,2 +1,41 @@
+
+/**
+ * Implements Runnable
+ * 实现 Runnable 接口 创建多线程
+ */
 public class ImplementsRunnable {
+
+    public static void main(String[] args) {
+        // new Thread 新建线程性能差 且 缺乏统一管理和更多的功能，
+        // 应使用 Java线程池，下面的写法仅用于练习，明日补齐 线程池 内容
+        new Thread(new Thread2("C")).start();
+        new Thread(new Thread2("D")).start();
+
+    }
+}
+
+class Thread2 implements Runnable {
+    private String name;
+
+    /**
+     * 构造器
+     * @param name
+     */
+    public Thread2(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 6; i++) {
+            System.out.println(String.format("%s 运行 ：%d ",name, i));
+
+            try {
+                // 为展示多线程，使用随机数休眠线程并输出 来只管展示多线程
+                Thread.sleep((int)(Math.random() * 10));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
